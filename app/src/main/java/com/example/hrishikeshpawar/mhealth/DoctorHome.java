2f273ff3e89c3665e3028d00f2c17dfbcd32ded3
hrishi1 <hrishikeshspawar@gmail.com>
2018-03-19 01:30:40 +0530

    Added UI for Doctor Home

---
+++ app/src/main/java/com/example/hrishikeshpawar/mhealth/DoctorHome.java
@@ -0,0 +1,13 @@
+package com.example.hrishikeshpawar.mhealth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DoctorHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_home);
+    }
+}
